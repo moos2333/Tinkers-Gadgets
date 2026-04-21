e4b6d86564b901ce25ccef1b969445029ea07283
moos2333 <xvruikai0804@126.com>
2026-04-21 11:53:34 +0800

    Update TinkersGadgets.java

--- src/main/java/com/npstra/tinkersgadgets/TinkersGadgets.java
+++ src/main/java/com/npstra/tinkersgadgets/TinkersGadgets.java
@@ -6,8 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = TinkersGadgets.MOD_ID, name = TinkersGadgets.MOD_NAME, version = TinkersGadgets.VERSION,
         dependencies = "required-after:tconstruct@[1.12.2-2.13.0.183,)")
@@ -15,7 +13,6 @@ public class TinkersGadgets {
     public static final String MOD_ID = "tinkersgadgets";
     public static final String MOD_NAME = "Tinkers Gadgets";
     public static final String VERSION = "0.0.1";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     @Mod.Instance(MOD_ID)
     public static TinkersGadgets instance;
