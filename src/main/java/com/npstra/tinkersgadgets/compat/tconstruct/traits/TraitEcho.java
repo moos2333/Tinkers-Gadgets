dc211b5814784cfa3ee1a9e338584b3ae03b8cf1
moos2333 <xvruikai0804@126.com>
2026-07-04 15:07:20 +0800

    update

--- src/main/java/com/npstra/tinkersgadgets/compat/tconstruct/traits/TraitEcho.java
+++ src/main/java/com/npstra/tinkersgadgets/compat/tconstruct/traits/TraitEcho.java
@@ -6,34 +6,21 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 import slimeknights.tconstruct.library.entity.EntityProjectileBase;
 import slimeknights.tconstruct.library.modifiers.ModifierAspect;
 import slimeknights.tconstruct.library.modifiers.ProjectileModifierTrait;
 import slimeknights.tconstruct.library.utils.TagUtil;
 
-import java.util.List;
-import java.util.UUID;
-
 public class TraitEcho extends ProjectileModifierTrait {
-    private static final String TAG_LAST_TARGET = "echo_last_target";
+    private static final String TAG_LAST_TARGET_ID = "echo_last_target_id";
     private static final String TAG_STACKS = "echo_stacks";
     private static final int MAX_STACKS = 5;
-    private static final float DAMAGE_PER_STACK = 0.5f;
-    private static final float SPLASH_DAMAGE_RATIO = 0.5f;
-    private static final double SPLASH_RADIUS = 1.5;
+    private static final float ECHO_DAMAGE_RATIO = 0.2f;
 
     public TraitEcho() {
         super("echo_throwingknife", 0xAA00AA);
         addAspects(ModifierAspect.projectileOnly);
-    }
-
-    @Override
-    public float damage(ItemStack tool, EntityLivingBase player, EntityLivingBase target, float damage, float newDamage, boolean isCritical) {
-        NBTTagCompound tag = TagUtil.getTagSafe(tool);
-        int stacks = tag.getInteger(TAG_STACKS);
-        return newDamage + stacks * DAMAGE_PER_STACK;
     }
 
     @Override
@@ -47,38 +34,26 @@ public class TraitEcho extends ProjectileModifierTrait {
 
         NBTTagCompound tag = TagUtil.getTagSafe(launcher);
         int stacks = tag.getInteger(TAG_STACKS);
-        String lastTargetStr = tag.getString(TAG_LAST_TARGET);
-        UUID lastTargetUUID = null;
-        if (!lastTargetStr.isEmpty()) {
-            try {
-                lastTargetUUID = UUID.fromString(lastTargetStr);
-            } catch (IllegalArgumentException ignored) {}
-        }
-
-        UUID currentTargetUUID = target.getUniqueID();
-        boolean sameTarget = lastTargetUUID != null && lastTargetUUID.equals(currentTargetUUID);
+        int lastId = tag.getInteger(TAG_LAST_TARGET_ID);
+        int currentId = target.getEntityId();
+        boolean sameTarget = lastId != 0 && lastId == currentId;
 
         if (sameTarget) {
             if (stacks > 0) stacks--;
         } else {
+            if (stacks > 0 && lastId != 0) {
+                Entity entity = world.getEntityByID(lastId);
+                if (entity instanceof EntityLivingBase && entity.isEntityAlive()) {
+                    float baseDamage = (float) impactSpeed * 1.5f;
+                    float extraDamage = baseDamage * stacks * ECHO_DAMAGE_RATIO;
+                    entity.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), extraDamage);
+                }
+            }
             if (stacks < MAX_STACKS) stacks++;
         }
 
-        tag.setString(TAG_LAST_TARGET, currentTargetUUID.toString());
+        tag.setInteger(TAG_LAST_TARGET_ID, currentId);
         tag.setInteger(TAG_STACKS, stacks);
         launcher.setTagCompound(tag);
-
-        if (stacks >= MAX_STACKS) {
-            float baseDamage = (float) impactSpeed * 1.5f;
-            float splashDamage = baseDamage * SPLASH_DAMAGE_RATIO;
-            AxisAlignedBB aabb = target.getEntityBoundingBox().grow(SPLASH_RADIUS);
-            List<EntityLivingBase> nearby = world.getEntitiesWithinAABB(EntityLivingBase.class, aabb,
-                    e -> e != target && e != attacker && e.isEntityAlive());
-            for (EntityLivingBase e : nearby) {
-                e.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), splashDamage);
-            }
-            tag.setInteger(TAG_STACKS, 0);
-            launcher.setTagCompound(tag);
-        }
     }
 }
